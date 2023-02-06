56d059220d9683547f210677b04c1ac12dc7b520
phuongthaotrinh <taenyisreal21091998@gmail.com>
2023-02-06 17:01:58 +0700

    deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -79,7 +79,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  callbackURL: "https://cinema-manager-eeiz.vercel.app/auth/google/callback",
   passReqToCallback: true
 },
   function (request, accessToken, refreshToken, profile, done) {
